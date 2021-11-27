f0d718e04b7816882c60a8adebe7bc8c3670764f
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-27 17:35:23 +0300

    update urls.py

--- salb_app/urls.py
+++ salb_app/urls.py
@@ -1,5 +1,18 @@
-from django.urls import path, include
+from django.urls import path
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path('',include('salb_app.urls'))
-# ]
+urlpatterns = [
+  path('',views.home,name='homePage'),
+  path('photos/',views.photos,name='allPhotos'),
+  path('photo/<int:photo_id>',views.detail,name='photos_item.detail'),
+  path('mombasa/',views.filter_mombasa_photos,name='mombasa'),
+  path('nairobi/',views.filter_campus_nairobi,name='nairobi'),
+  path('Johannesburg/',views.filter_Johannesburg_photos,name='Johannesburg'),
+  path('narok/',views.filter_narok_photos,name='narok'),
+  path('search/',views.search_photos_category, name='searchPhotos')
+]
+
+if settings.DEBUG:
+  urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
