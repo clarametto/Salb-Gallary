b527257451a65ab5fd9c6901d83225325ab5af8d
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-29 15:56:12 +0300

    add home photos and search

--- salb_app/views.py
+++ salb_app/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse, Http404
-from datetime import dt
-from.models import PersonalInfo,Photo,Category,Location
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Photo,Location,Category
 from django.core.exceptions import ObjectDoesNotExist
 
-
 # Create your views here.
+def home(request):
+  return render(request, 'index.html')
+
+def search_photos_category(request):
+  if 'photo' in request.GET and request.GET["photo"]:
+    search_term = request.GET.get("photo")
+    searched_photos = Photo.search_photos_by_category(search_term)
+    message = f"{search_term}"
+
+    return render(request, 'search.html', {"message":message,"photos":searched_photos})
+
+  else:
+    message = 'You have not searched for any term'
+    return render(request, 'search.html', {"message":message})
+  
+
+def photos(request):
+  photos =Photo.objects.all().order_by("-posted_at")
+  location = Location.objects.all()
+  return render(request,'photos.html',{'photos':photos, 'location':location})
+
